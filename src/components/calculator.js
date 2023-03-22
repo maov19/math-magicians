5daca2902eee362bb92909092475fad4eef7ffb9
mossa <martin.ossa.8@gmail.com>
2023-03-22 17:40:12 +0100

    Add calculator functionalities

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,32 +1,46 @@
-import Zero from './zero';
+/* eslint-disable */
+
+import React, { useState } from 'react';
+import Big from 'big.js';
+import calculate from '../logic/calculate';
+import operate from '../logic/operate';
 
 function Calculator() {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null
+  });
+
+  const handleClick = buttonName => {
+    const newState = calculate(state, buttonName);
+    setState(newState);
+  };
+
   return (
     <div className="calculator-grid">
-      <div className="output">
-        <div className="current-operand">0</div>
-      </div>
-      <button type="button">AC</button>
-      <button type="button">+/-</button>
-      <button type="button">%</button>
-      <button type="button" className="orange-button">+</button>
-      <button type="button">7</button>
-      <button type="button">8</button>
-      <button type="button">9</button>
-      <button type="button" className="orange-button">x</button>
-      <button type="button">4</button>
-      <button type="button">5</button>
-      <button type="button">6</button>
-      <button type="button" className="orange-button">-</button>
-      <button type="button">1</button>
-      <button type="button">2</button>
-      <button type="button">3</button>
-      <button type="button" className="orange-button">+</button>
-      <Zero />
-      <button type="button">.</button>
-      <button type="button" className="orange-button">=</button>
+      <div className="output">{state.next || state.total}</div>
+        <button type="button" onClick={() => handleClick('AC')}>AC</button>
+        <button type="button" onClick={() => handleClick('+/-')}>+/-</button>
+        <button type="button" onClick={() => handleClick('%')}>%</button>
+        <button type="button" onClick={() => handleClick('÷')} className="orange-button">÷</button>
+        <button type="button" onClick={() => handleClick('7')}>7</button>
+        <button type="button" onClick={() => handleClick('8')}>8</button>
+        <button type="button" onClick={() => handleClick('9')}>9</button>
+        <button type="button" onClick={() => handleClick('x')} className="orange-button">x</button>
+        <button type="button" onClick={() => handleClick('4')}>4</button>
+        <button type="button" onClick={() => handleClick('5')}>5</button>
+        <button type="button" onClick={() => handleClick('6')}>6</button>
+        <button type="button" onClick={() => handleClick('-')} className="orange-button">-</button>
+        <button type="button" onClick={() => handleClick('1')}>1</button>
+        <button type="button" onClick={() => handleClick('2')}>2</button>
+        <button type="button" onClick={() => handleClick('3')}>3</button>
+        <button type="button" onClick={() => handleClick('+')} className="orange-button">+</button>
+        <button type="button" onClick={() => handleClick('0')} className="span-two">0</button>
+        <button type="button" onClick={() => handleClick('.')} >.</button>
+        <button type="button" onClick={() => handleClick('=')} >=</button>
     </div>
-  );
-}
+    );
+  }
 
-export default Calculator;
+  export default Calculator;
