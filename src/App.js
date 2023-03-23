d019bffeada8292e25c89e081169f44af375d80d
mossa <martin.ossa.8@gmail.com>
2023-03-23 14:17:36 +0100

    Add quote to main display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+/* eslint-disable */
+
 import './index.css';
 import Calculator from './components/calculator';
+import Quote from './components/quote';
 
 function App() {
   return (
+    <div class='main-container'>
     <Calculator />
+    <Quote />
+    </div>
   );
 }
 
