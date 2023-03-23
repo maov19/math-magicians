1c93edf2b366363c20b253958336b103504e9a2e
mossa <martin.ossa.8@gmail.com>
2023-03-23 14:03:00 +0100

    Add random quote module

---
+++ src/components/quote.js
@@ -0,0 +1,58 @@
+/* eslint-disable */
+
+import React, { useState, useEffect } from "react";
+
+const category = 'funny';
+const url = "https://api.api-ninjas.com/v1/quotes?category=" + category;
+const key = "8d3AEu5Zw1oN3/7ZcRczQw==Pttifja0qKZnwTuq";
+
+
+function Quote() {
+
+// state for quote
+  const [quote, setQuote] = useState(""); 
+// state for loading 
+  const [loading, setLoading] = useState(true);
+// state for error  
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+        setLoading(true);
+        try {
+            const response = await fetch(
+                url,
+                {
+                headers: {
+                    'X-Api-Key': key,
+                },
+                }
+            );
+            const data = await response.json();
+            setQuote(data[0].quote);
+        } catch (error) {
+            setError(error)
+        } finally {
+            setLoading(false);
+        }
+    }
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div id="quote-container">
+      <h1>Random funny quote:</h1>
+      <h2 id="quote">{quote}</h2>
+    </div>
+  );
+}
+
+export default Quote;
