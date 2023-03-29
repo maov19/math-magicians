9f2e4e1d48e472ec46af851bcc11d2bbf6673faa
mossa <martin.ossa.8@gmail.com>
2023-03-29 15:13:52 +0200

    Add unit testing for operate and calculate functions

---
+++ src/tests/logic.test.js
@@ -0,0 +1,25 @@
+import calculate from '../logic/calculate';
+import operate from '../logic/operate';
+
+describe('calculate testing', () => {
+  it('should return an object with null values for total, next, and operation when buttonName is "AC"', () => {
+    const result = calculate({}, 'AC');
+    expect(result).toEqual({ total: null, next: null, operation: null });
+  });
+
+  it('should perform calculation when buttonName is "="', () => {
+    const result = calculate({ total: '2', next: '3', operation: '+' }, '=');
+    expect(result).toEqual({ total: '5', next: null, operation: null });
+  });
+})
+describe('operate testing', () => {
+    it('should return a sum if "+" is assigned between two numbers', () => {
+      const result = operate(1, 5, '+');
+      expect(result).toEqual('6');
+    });
+  
+    it('should return a multiplication if "x" is assigned between two numbers', () => {
+        const result = operate(10, 3, 'x');
+        expect(result).toEqual('30');
+      });
+  })
