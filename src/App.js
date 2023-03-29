77b10408cf14ffc6ee068f8a1146d6a935616478
mossa <martin.ossa.8@gmail.com>
2023-03-29 12:37:17 +0200

    Add navbar to main page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-/* eslint-disable */
-
-import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
-import './index.css';
+import {
+  BrowserRouter, Routes, Route, Outlet,
+} from 'react-router-dom';
 import Calculator from './components/calculator';
 import Quote from './components/quote';
+import Home from './components/Home';
 
 function Layout() {
   return (
     <>
       <Outlet />
     </>
-  )
+  );
 }
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Layout />}>
-          <Route index element={<div>Home page</div>} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
           <Route path="calculator" element={<Calculator />} />
           <Route path="quote" element={<Quote />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
